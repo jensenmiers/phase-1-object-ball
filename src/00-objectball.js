a5751dbaf8819981ff2cd49015d229665985da44
jensenmiers <jensenmiers@gmail.com>
2022-12-14 16:35:46 -0800

    Co-authored-by: Sean3344 <Sean3344@users.noreply.github.com>

---
+++ src/00-objectball.js
@@ -0,0 +1,40 @@
+function gameObject() {
+ const team = {
+    home: {
+        teamName: "Brooklyn Nets",
+        colors: ['Black','White'] ,
+        players: { 
+          'Alan Iverson' : {
+            number: '0',
+            shoe: '16',
+            points: '22',
+            rebounds: '12',
+            assits: '12',
+            steals: '3',
+            blocks: '1',
+            slamDunks: '1',}
+          },
+      },
+    away: {
+      teamName: 'Charlotte Hornets',
+      colors: ['Turquoise','Purple'],
+      players: {
+        'Jeff Adrien' : {
+          number: '4',
+          shoe: '18',
+          points: '10',
+          rebounds: '1',
+          assists: '1',
+          steals: '2',
+          blocks: '7',
+          slamDunks: '2',}
+      },
+    }
+  }
+}
+  return team
+
+    
+
+
+// 
